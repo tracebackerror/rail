aa3e31188aed1c6176e8f140326a4426f0a01827
Shivam Chaurasia <SHIVAM9172@GMAIL.COM>
2022-02-06 03:43:22 +0530

    Init

---
+++ rail_frontend/src/App.js
@@ -0,0 +1,15 @@
+import { BiArchive } from "react-icons/bi";
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl">
+      <BiArchive className="inline-block text-red-400 align-top" />Appointment  </h1>
+    </div>
+  );
+}
+
+export default App;
