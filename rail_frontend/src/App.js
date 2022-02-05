471743373a65fd3262a0e15f171f213d1c3680e6
Shivam Chaurasia <SHIVAM9172@GMAIL.COM>
2022-02-06 04:14:35 +0530

    Minor Modification

--- rail_frontend/src/App.js
+++ rail_frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BiArchive } from "react-icons/bi";
+import { MdOutlineDirectionsRailway } from "react-icons/md";
 import './App.css';
 import Search from './components/Search';
 import Card from './components/card';
@@ -8,8 +8,9 @@ function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl">
-      <BiArchive className="inline-block text-red-400 align-top" />Appointment  </h1>
+      <MdOutlineDirectionsRailway className="inline-block text-red-400 align-top" />Railway Booking  </h1>
       <Search/>
+      <br/>
       <Card/>
     </div>
   );
--- rail_frontend/src/components/card.js
+++ rail_frontend/src/components/card.js
@@ -1,4 +1,5 @@
 import { BiSearch, BiCaretDown, BiCheck } from "react-icons/bi"
+import { MdDirectionsRailwayFilled } from "react-icons/md"
 import { useState } from 'react';
 import logo from "../logo.svg";
 
@@ -7,11 +8,14 @@ const Card = () => {
   return (
     <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
         <div class="shrink-0">
-            <img class="h-12 w-12" src={logo} alt="ChitChat Logo" width="100" height="50"/>
+            
+            <MdDirectionsRailwayFilled class="h-12 w-12" src={logo} alt="ChitChat Logo" width="100" height="50"/>
         </div>
         <div>
-            <div class="text-xl font-medium text-black">ChitChat</div>
-            <p class="text-slate-500">You have a new message!</p>
+            <div class="text-xl font-medium text-black">Shatabdi Super Fast Express</div>
+            <p class="text-slate-500">Available Seats 20 </p>
+            <i></i>
+            <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Book Now</button>
         </div>
     </div>
   )
