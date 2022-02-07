79dd3a4739aad41a021d32d55c9ee1da3a16e5e3
Shivam Chaurasia <SHIVAM9172@GMAIL.COM>
2022-02-07 18:52:32 +0530

    button

--- rail_frontend/src/components/AppointmentInfo.js
+++ rail_frontend/src/components/AppointmentInfo.js
@@ -1,11 +1,29 @@
 import { BiTrash } from "react-icons/bi"
+import Cookies from 'js-cookie'
+import { Link } from "react-router-dom";
+
+
+
 
 const AppointmentInfo = ({ appointment, onDeleteAppointment }) => {
+
+    let delete_button;
+    let book_now;
+    if (Cookies.get('username')){
+        delete_button = <button onClick={() => onDeleteAppointment(appointment.id)} type="button"
+        className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+        <BiTrash /></button>;
+
+        book_now = <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"><Link to="/bookTrain">Book Now</Link></button>;
+        
+    
+    }else{
+        delete_button = '';
+    };
+
   return (
     <li className="px-3 py-3 flex items-start">
-      <button onClick={() => onDeleteAppointment(appointment.id)} type="button"
-        className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-        <BiTrash /></button>
+      {delete_button}
       <div className="flex-grow">
         <div className="flex items-center">
           <span className="flex-none font-medium text-2xl text-blue-500">{appointment.petName}</span>
@@ -13,6 +31,7 @@ const AppointmentInfo = ({ appointment, onDeleteAppointment }) => {
         </div>
         <div><b className="font-bold text-blue-500">Owner:</b> {appointment.ownerName}</div>
         <div className="leading-tight">{appointment.aptNotes}</div>
+        {book_now}
       </div>
     </li>
   )
