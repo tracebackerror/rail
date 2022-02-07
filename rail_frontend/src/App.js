49d5c1b1bee20e4f272f1dd0a5ff8bda3c238244
Shivam Chaurasia <SHIVAM9172@GMAIL.COM>
2022-02-07 18:52:01 +0530

    Button

--- rail_frontend/src/App.js
+++ rail_frontend/src/App.js
@@ -1,9 +1,23 @@
 import { useState, useEffect, useCallback } from 'react'
-import { BiCalendar } from "react-icons/bi"
+import { BiCalendar, BiLogIn, BiLogOut } from "react-icons/bi"
 import { GiRailRoad } from "react-icons/gi";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Search from "./components/Search"
 import AddAppointment from "./components/AddAppointment"
 import AppointmentInfo from "./components/AppointmentInfo"
+import Cookies from 'js-cookie'
+
+
+function logOut() {
+  Cookies.remove('username');
+  window.location.reload(false);
+}
+
+function logIn() {
+  Cookies.set('username', 'value');
+  window.location.reload(false);
+}
+
 
 function App() {
 
@@ -11,6 +25,8 @@ function App() {
   let [query, setQuery] = useState("");
   let [sortBy, setSortBy] = useState("petName");
   let [orderBy, setOrderBy] = useState("asc");
+
+  
 
   const filteredAppointments = appointmentList.filter(
     item => {
@@ -39,38 +55,50 @@ function App() {
   useEffect(() => {
     fetchData()
   }, [fetchData]);
+  let button;
+
+  if (Cookies.get('username')){
+    button = <button onClick={logOut} class="inline-block text-red-400 align-top text-right"><BiLogOut className="inline-block text-red-400 align-top text-right"/></button>;
+ 
+  }else{
+    button = <button onClick={logIn} class="inline-block text-red-400 align-top text-right"><BiLogIn className="inline-block text-red-400 align-top text-right"/></button>;
+  };
 
   return (
-    <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl mb-3">
-        <GiRailRoad className="inline-block text-red-400 align-top" />Online Rail Reservation</h1>
-      <AddAppointment
-        onSendAppointment={myAppointment => setAppointmentList([...appointmentList, myAppointment])}
-        lastId={appointmentList.reduce((max, item) => Number(item.id) > max ? Number(item.id) : max, 0)}
-      />
-      <Search query={query}
-        onQueryChange={myQuery => setQuery(myQuery)}
-        orderBy={orderBy}
-        onOrderByChange={mySort => setOrderBy(mySort)}
-        sortBy={sortBy}
-        onSortByChange={mySort => setSortBy(mySort)}
-      />
+      <div className="App container mx-auto mt-3 font-thin">
+        <h1 className="text-5xl mb-3">
+          <GiRailRoad className="inline-block text-red-400 align-top" />{Cookies.get('username') ? Cookies.get('username'): 0} Online Rail Reservation 
+          {button}
+          </h1>
+        <AddAppointment
+          onSendAppointment={myAppointment => setAppointmentList([...appointmentList, myAppointment])}
+          lastId={appointmentList.reduce((max, item) => Number(item.id) > max ? Number(item.id) : max, 0)}
+        />
+        <Search query={query}
+          onQueryChange={myQuery => setQuery(myQuery)}
+          orderBy={orderBy}
+          onOrderByChange={mySort => setOrderBy(mySort)}
+          sortBy={sortBy}
+          onSortByChange={mySort => setSortBy(mySort)}
+        />
 
-      <ul className="divide-y divide-gray-200">
-        {filteredAppointments
-          .map(appointment => (
-            <AppointmentInfo key={appointment.id}
-              appointment={appointment}
-              onDeleteAppointment={
-                appointmentId =>
-                  setAppointmentList(appointmentList.filter(appointment =>
-                    appointment.id !== appointmentId))
-              }
-            />
-          ))
-        }
-      </ul>
+        <ul className="divide-y divide-gray-200">
+          {filteredAppointments
+            .map(appointment => (
+              <AppointmentInfo key={appointment.id}
+                appointment={appointment}
+                onDeleteAppointment={
+                  appointmentId =>
+                    setAppointmentList(appointmentList.filter(appointment =>
+                      appointment.id !== appointmentId))
+                }
+              />
+            ))
+          }
+        </ul>
+        
     </div>
+     
   );
 }
 
